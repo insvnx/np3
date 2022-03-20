b4c463d03ea84be057ebf73e0d392883421bcb51
insvnx@gmail.com <insvnx@gmail.com>
2022-03-20 06:11:39 -0500

    update

--- np3.py
+++ np3.py
@@ -1,28 +1,29 @@
-#
 # np3 by INSVNX
-#
 # requires hexchat / python3 / vlc
-#   add http iface in vlc
+#   add http iface and set password in vlc
 #   modify 'password' field below
-#
 # /np3 - spams chan with current song
-# 
 import urllib.request
 import xml.etree.ElementTree
+import html
+from html import unescape
 import re
 import hexchat
 
 __author__ = "INSVNX"
 __module_name__ = "np3"
-__module_version__ = "3.0"
+__module_version__ = "4.2.0"
 __module_description__ = "Now Playing 3"
 
+def format_case(a):
+    return a.group(1) + a.group(2).upper()
+
 def on_np3(word, word_eol, userdata):
-	vlcurl = 'http://[::1]:8080/requests/status.xml'
+	vlcurl = 'http://localhost:8080/requests/status.xml'
 	vlcrealm = 'VLC stream'
-	location = 'http://[::1]:8080/' 
+	location = 'http://localhost:8080/'
 	username = ''
-	password = 'teaminsane'
+	password = 'CHANGETHISPASSWORD'
 	auth = urllib.request.HTTPBasicAuthHandler()
 	auth.add_password(vlcrealm, location, username, password)
 	opener = urllib.request.build_opener(auth)
@@ -32,24 +33,51 @@ def on_np3(word, word_eol, userdata):
 	root = tree.getroot()
 
 	for artist in root.findall(".//*[@name='artist']"):
-	  artist = artist.text
+	  scrubbed = html.unescape(artist.text)
+	  clean = re.sub(r'_', ' ', scrubbed)
+	  format = re.sub("(^|\s)(\S)", format_case, clean)
+	  artist = format
+
+	for album in root.findall(".//*[@name='album']"):
+	  scrubbed = html.unescape(album.text)
+	  clean = re.sub(r'_', ' ', scrubbed)
+	  format = re.sub("(^|\s)(\S)", format_case, clean)
+	  album = format
 
 	for title in root.findall(".//*[@name='title']"):
-	  title = title.text
+	  scrubbed = html.unescape(title.text)
+	  clean = re.sub(r'_', ' ', scrubbed)
+	  format = re.sub("(^|\s)(\S)", format_case, clean)
+	  title = format
+
+	for genre in root.findall(".//*[@name='genre']"):
+	  scrubbed = html.unescape(genre.text)
+	  clean = re.sub(r'_', ' ', scrubbed)
+	  format = re.sub("(^|\s)(\S)", format_case, clean)
+	  genre = format
+
+	for date in root.findall(".//*[@name='date']"):
+	  date = int(date.text)
+	  year = str(date)
 
 	for length in root.findall('length'):
-	  t = int(length.text)
-	  m = t // 60
-	  s = t - m * 60
-	  minutes = str(m)
-	  seconds = str(s)
+	  time = int(length.text)
+	  division = time // 60
+	  subtraction = time - division * 60
+	  minutes = str(division)
+	  seconds = str(subtraction)
 
 	for bitrate in root.findall(".//*[@name='Bitrate']"):
-	  bits = re.sub(r' kb/s', 'kb/s', bitrate.text)
+	  format = re.sub(r' kb/s', '', bitrate.text)
+	  vbr_check = int(format)
+	  if vbr_check < 128:
+	    bitrate = str(256)
+	  else:
+	    bitrate = str(vbr_check) 
 
-	hexchat.command("say np3("+artist+" - "+title+")("+minutes+"m"+seconds+"s/"+bits+")")
+	hexchat.command("say mp3("+artist+"-"+album+"-"+title+"-"+year+")("+minutes+"m"+seconds+"s/"+bitrate+"kb/s)")
 	return hexchat.EAT_ALL
 
 hexchat.hook_command("np3", on_np3, userdata=hexchat.get_info('channel'))
 
-hexchat.prnt(__module_name__ + ' by ' + __author__ + ' loaded.')
+hexchat.prnt(__module_name__ + ' ' + __module_version__ + ' by ' + __author__ + ' loaded.')
