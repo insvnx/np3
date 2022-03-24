fc0f15149b36dfcabf8e5cc6209520785ca614e3
insvnx@gmail.com <insvnx@gmail.com>
2022-03-24 04:57:51 -0500

    updated indentation

--- np3.py
+++ np3.py
@@ -20,81 +20,81 @@ VLC_PASSWORD = "SET_THIS_PASSWORD_IN_VLC"
 #########################################
 
 def format_case(a):
-    return a.group(1) + a.group(2).upper()
+  return a.group(1) + a.group(2).upper()
 
 def on_np3(word, word_eol, userdata):
-	vlcurl = 'http://localhost:8080/requests/status.xml'
-	vlcrealm = 'VLC stream'
-	location = 'http://localhost:8080/'
-	username = ''
-	password = VLC_PASSWORD 
-	auth = urllib.request.HTTPBasicAuthHandler()
-	auth.add_password(vlcrealm, location, username, password)
-	opener = urllib.request.build_opener(auth)
-	urllib.request.install_opener(opener)
-	status = urllib.request.urlopen(vlcurl)
-	tree = xml.etree.ElementTree.parse(status)
-	root = tree.getroot()
+  vlcurl = 'http://localhost:8080/requests/status.xml'
+  vlcrealm = 'VLC stream'
+  location = 'http://localhost:8080/'
+  username = ''
+  password = VLC_PASSWORD 
+  auth = urllib.request.HTTPBasicAuthHandler()
+  auth.add_password(vlcrealm, location, username, password)
+  opener = urllib.request.build_opener(auth)
+  urllib.request.install_opener(opener)
+  status = urllib.request.urlopen(vlcurl)
+  tree = xml.etree.ElementTree.parse(status)
+  root = tree.getroot()
 
-	for artist in root.findall(".//*[@name='artist']"):
-	  scrub = html.unescape(artist.text)
-	  clean = re.sub(r'_', ' ', scrub)
-	  format = re.sub("(^|\s)(\S)", format_case, clean)
-	  build = format
-	  artist = build
+  for artist in root.findall(".//*[@name='artist']"):
+    scrub = html.unescape(artist.text)
+    clean = re.sub(r'_', ' ', scrub)
+    format = re.sub("(^|\s)(\S)", format_case, clean)
+    build = format
+    artist = build
 
-	for album in root.findall(".//*[@name='album']"):
-	  scrub = html.unescape(album.text)
-	  clean = re.sub(r'_', ' ', scrub)
-	  format = re.sub("(^|\s)(\S)", format_case, clean)
-	  build = format
-	  album = build 
+  for album in root.findall(".//*[@name='album']"):
+    scrub = html.unescape(album.text)
+    clean = re.sub(r'_', ' ', scrub)
+    format = re.sub("(^|\s)(\S)", format_case, clean)
+    build = format
+    album = build 
 
-	for title in root.findall(".//*[@name='title']"):
-	  scrub = html.unescape(title.text)
-	  clean = re.sub(r'_', ' ', scrub)
-	  format = re.sub("(^|\s)(\S)", format_case, clean)
-	  build = format
-	  title = build 
+  for title in root.findall(".//*[@name='title']"):
+    scrub = html.unescape(title.text)
+    clean = re.sub(r'_', ' ', scrub)
+    format = re.sub("(^|\s)(\S)", format_case, clean)
+    build = format
+    title = build 
 
-	for genre in root.findall(".//*[@name='genre']"):
-	  scrub = html.unescape(genre.text)
-	  clean = re.sub(r'_', ' ', scrub)
-	  format = re.sub("(^|\s)(\S)", format_case, clean)
-	  build = format
-	  genre = build 
+  for genre in root.findall(".//*[@name='genre']"):
+    scrub = html.unescape(genre.text)
+    clean = re.sub(r'_', ' ', scrub)
+    format = re.sub("(^|\s)(\S)", format_case, clean)
+    build = format
+    genre = build 
 
-	for date in root.findall(".//*[@name='date']"):
-	  date = int(date.text)
-	  build = str(date) 
-	  year = build 
+  for date in root.findall(".//*[@name='date']"):
+    date = int(date.text)
+    build = str(date) 
+    year = build 
 
-	for length in root.findall('length'):
-	  time = int(length.text)
-	  division = time // 60
-	  build = str(division)
-	  minutes = build
+  for length in root.findall('length'):
+    time = int(length.text)
+    division = time // 60
+    build = str(division)
+    minutes = build
 
-	for length in root.findall('length'):
-	  time = int(length.text)
-	  division = time // 60
-	  subtraction = time - division * 60
-	  build = str(subtraction)
-	  seconds = build
+  for length in root.findall('length'):
+    time = int(length.text)
+    division = time // 60
+    subtraction = time - division * 60
+    build = str(subtraction)
+    seconds = build
 
-	for bitrate in root.findall(".//*[@name='Bitrate']"):
-	  format = re.sub(r' kb/s', '', bitrate.text)
-	  vbr_check = int(format)
-	  if vbr_check < 128:
-	    build = str(256)
-	    bitrate = build
-	  else:
-	    build = str(vbr_check)
-	    bitrate = build
+  for bitrate in root.findall(".//*[@name='Bitrate']"):
+    format = re.sub(r' kb/s', '', bitrate.text)
+    vbr_check = int(format)
+    if vbr_check < 128:
+      build = str(256)
+      bitrate = build
+    else:
+      build = str(vbr_check)
+      bitrate = build
 
-	hexchat.command("say mp3("+artist+"-"+album+"-"+title+"-"+year+")("+minutes+"m"+seconds+"s/"+bitrate+"kb/s)")
+  hexchat.command("say mp3("+artist+"-"+album+"-"+title+"-"+year+")("+minutes+"m"+seconds+"s/"+bitrate+"kb/s)")
 
-	return hexchat.EAT_ALL
+  return hexchat.EAT_ALL
 
 __author__ = "INSVNX"
 __module_name__ = "np3"
